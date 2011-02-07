312c955e49814ade571b9da172923d9a4a8788fc
Moses Nakamura <nnythm@gmail.com>
2011-02-06 20:22:02 -0500

    first commit, with remnants from old project

---
+++ board/board.py
@@ -0,0 +1,83 @@
+import cherrypy
+import pymongo
+from pymongo import Connection
+from Cheetah.Template import Template
+from post import Post
+import post as p
+from datetime import datetime
+
+#This class connects the backend with the frontend by accessing data and filling the templates
+class Board:
+    def __init__(self,board_name="FroSci"):
+        userID = 'admin'
+        pwd = 'hackcu11'
+        host= 'flame.mongohq.com'
+        port = 27039
+        dbName = 'posts_database'
+        connection=Connection(host,port)
+        #name of database
+        db = connection[dbName]
+        db.authenticate(userID, pwd)
+
+        self.courses=list(db.courses.find())
+        self.board_name = board_name
+        #Connect and load posts
+        self.posts = p.get_posts(self.board_name)
+
+        #load templates
+        index_t = open('templates/index.tmpl', 'r')
+        expand_t = open('templates/expand.tmpl', 'r')
+        homepage_t = open('templates/homepage.tmpl', 'r')
+        #string-ize
+        self.expand_template = expand_t.read()
+        self.index_template = index_t.read()
+        self.homepage_template = homepage_t.read()
+
+    def index(self, message=None):
+        name_space={"courses" : self.courses}
+
+        return str(Template(self.homepage_template, name_space))
+#        return "whatever"
+    index.exposed = True
+
+
+    def course(self, board ,message=None):
+        if cherrypy.request.method == 'POST':
+            if not message:
+                print('No message received')
+                pass #Add to errors or something...
+            else:
+                #adds a post
+                self.add_post(message)
+
+        #Connect and load posts
+                self.posts = p.get_posts(self.board_name)
+
+        self.board_name = board
+
+        self.posts = p.get_posts(self.board_name)
+
+        name_space = {'posts':self.posts}
+        return str(Template(self.index_template, name_space))
+    course.exposed = True
+    
+    def expand(self, post_id, message=None):
+        post = Post(self.board_name, post_id=post_id)
+        if cherrypy.request.method == 'POST':
+            if not message:
+                print('Message was empty')
+                pass #Add to errors or something...
+            else:
+                post.add_reply(message)
+        post.update()
+        #display the post and responses for post_id
+        post = Post(self.board_name, post_id=post_id)
+        #Render the page
+           
+        name_space = {'post':post}
+        return str(Template(self.expand_template, name_space))
+    expand.exposed = True
+
+    def add_post(self,message):
+        post = Post(self.board_name, message=message)
+        
---
+++ board/post.py
@@ -0,0 +1,96 @@
+import pymongo
+import cherrypy
+from pymongo import Connection
+import cgi #For html escaping
+import time
+from datetime import datetime
+from pymongo.objectid import ObjectId
+
+num_saved=25
+
+#TODO:
+#implement author functionality
+
+class Post:
+    #inits
+    def __init__(self,board,post_id="",message="",author=""):
+        userID = 'admin'
+        pwd = 'hackcu11'
+        host= 'flame.mongohq.com'
+        port = 27039
+        dbName = 'posts_database'
+        connection=Connection(host,port)
+        #name of database
+        db = connection[dbName]
+        db.authenticate(userID, pwd)
+
+        self.collection=db.posts
+        self.board=board
+        if post_id == "":
+            self.timestamp=time.time()
+            self.post_id=self.collection.insert({'message':cgi.escape(message),'board':self.board,'replies':[],'timestamp':self.timestamp,'author':author})
+            tmp=self.collection.find().sort('timestamp')
+            tmp=(list(tmp))
+            if(len(tmp)>num_saved):
+                self.collection.remove({'_id':tmp[0]['_id']})
+            self.replies=[]
+            self.author=author
+        else:
+            #needs ObjectId to string->weird objectid type for mongo
+            dic = db.posts.find_one({'_id': ObjectId(post_id) })
+            self.message = dic['message']
+            self.replies = dic['replies']
+            self.post_id = dic['_id']
+            self.author = dic['author']
+        self.min_repls=self.get_mini_replies()
+
+
+    #refreshes the state of the post
+    def update(self):
+        coll_dict = self.collection.find_one({'_id': self.post_id})
+        self.replies = coll_dict['replies']
+        self.message = coll_dict['message']
+        self.timestamp = coll_dict['timestamp']
+
+    def get_mini_replies(self):
+        return self.replies[:3]
+
+    #adds a reply atomicly
+    def add_reply(self,new_reply,author=""):
+        self.timestamp = time.time()
+        self.replies.append({"reply-text":cgi.escape(new_reply),"timestamp":datetime.now(),"replies":[],"author":author})
+        self.collection.update({'_id' : self.post_id},{"$set" : {"replies" :self.replies,"timestamp" : self.timestamp}})
+
+    def get_timestamp():
+        return self.timestamp
+
+def get_posts(board):
+    host= 'flame.mongohq.com'
+    port = 27039
+    dbName = 'posts_database'
+    connection=Connection(host,port)
+    userID = 'admin'
+    pwd = 'hackcu11'
+
+    #name of database
+    db = connection[dbName]
+    db.authenticate(userID, pwd)
+    posts=db.posts
+    #print [post for post in posts.find({'board':board})]
+    print [str(p['_id']) for p in posts.find({'board':board})] 
+    return [Post(board,post_id=post['_id']) for post in posts.find({'board':board})][::-1]
+
+class Reply(Post):
+    def __init__(self,message,replies=[],timestamp=datetime.now(),author=""):
+        self.replies = replies
+        self.message = message
+        self.timestamp = timestamp
+        self.author = author
+
+    def update(self):
+        pass
+
+    #adds a reply atomicly
+    def add_reply(self,new_reply,author=""):
+        self.replies.append({"reply-text":cgi.escape(new_reply),"timestamp":datetime.now(),"replies":[],"author":author})
+        self.collection.update({'_id' : self.post_id},{"$set" : {"replies" :self.replies}})
